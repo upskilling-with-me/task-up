12ad919a3de493b31f76cf50a489ed5c1e507418
Sankalp Jain <sankalp.jain@clario.com>
2024-11-21 16:45:22 +0000

    Adeded header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import "./App.css";
+
 /*
 Header Component
   Title
@@ -22,8 +25,34 @@ Footer Component
   Clear All Todos Button
 */
 
-function App() {
-	return <h1>Welcome to Task Up</h1>;
+type Theme = "light" | "dark";
+interface HeaderProps {
+	theme: Theme;
+	setTheme: React.Dispatch<React.SetStateAction<Theme>>;
 }
+
+const Header: React.FC<HeaderProps> = ({ theme, setTheme }) => {
+	const handleThemeChange = () => {
+		setTheme(theme === "light" ? "dark" : "light");
+	};
+
+	return (
+		<div className="header">
+			<h1>Task Us</h1>
+			<button type="button" onClick={handleThemeChange}>
+				{theme}
+			</button>
+		</div>
+	);
+};
+
+const App: React.FC = () => {
+	const [theme, SetTheme] = useState<Theme>("light");
+	return (
+		<div>
+			<Header theme={theme} setTheme={SetTheme} />
+		</div>
+	);
+};
 
 export default App;
