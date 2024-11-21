382d3b0c301089dbc8d1efc9ed4a12f0fbd12bd7
Sankalp Jain <sankalp.jain@clario.com>
2024-11-21 14:30:07 +0000

    Added high level component plan

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,27 @@
+/*
+Header Component
+  Title
+  Theme
+
+AddTodo Component
+  Input Field
+  priority
+  Add Button
+
+TodoList Component
+  Checkbox
+  priority
+  Description
+  Edit Button
+  Delete Button
+
+Footer Component
+  Total Todos
+  Filter Todos Button
+  Sort Todos Button
+  Clear All Todos Button
+*/
+
 function App() {
 	return <h1>Welcome to Task Up</h1>;
 }
