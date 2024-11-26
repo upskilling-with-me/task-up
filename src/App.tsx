c9db46326ae60f65de61212dee753c98712136a1
Sankalp Jain <sankalp.jain@clario.com>
2024-11-26 09:57:47 +0000

    Added layout of todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import "./App.css";
-import { Button } from "@mui/material";
+import {
+	Button,
+	Checkbox,
+	TextField,
+} from "@mui/material";
 
 /*
 Header Component
@@ -9,12 +13,13 @@ Header Component
 
 AddTodo Component
   Input Field
-  priority
   Add Button
 
 TodoList Component
+  List of Todos
+
+Todo Component
   Checkbox
-  priority
   Description
   Edit Button
   Delete Button
@@ -27,6 +32,8 @@ Footer Component
 */
 
 type Theme = "light" | "dark";
+type TodoStatus = "todo" | "done";
+
 interface HeaderProps {
 	theme: Theme;
 	setTheme: React.Dispatch<React.SetStateAction<Theme>>;
@@ -52,13 +59,64 @@ const Header: React.FC<HeaderProps> = ({ theme, setTheme }) => {
 	);
 };
 
+interface Todo {
+	id: string;
+	description: string;
+	status: TodoStatus;
+}
+
+interface TodoItemProps {
+	description: string;
+	status: TodoStatus;
+}
+
+const TodoItem: React.FC<TodoItemProps> = ({ description, status }) => {
+	return (
+		<div className="todo-card">
+			<Checkbox checked={status == 'done'} />
+			<span>{status}</span>
+			<TextField label={description} />
+			<Button variant="contained" color="error">
+				delete
+			</Button>
+		</div>
+	);
+};
+
+const TODO_LIST: Todo[] = [
+	{ id: '1', description: "Buy Grocery", status: "todo" },
+	{ id: '2', description: "Go for a run", status: "todo" },
+	{ id: '3', description: "Do dishes", status: "todo" },
+	{ id: '4', description: "Buy milk", status: "done" },
+];
+
+interface TodoListProps {
+	todos: Todo[];
+}
+
+const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+	return (
+		<div className="container">
+			<ul className="todo-list-card">
+				{todos.map((todo) => (
+					<li key={todo.id}>
+						<TodoItem description={todo.description} status={todo.status} />
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+};
 
 
 const App: React.FC = () => {
 	const [theme, SetTheme] = useState<Theme>("light");
+	const [todoList, SetTodoList] = useState(TODO_LIST);
+
 	return (
 		<div>
 			<Header theme={theme} setTheme={SetTheme} />
+			<TodoList todos={todoList} />
 		</div>
 	);
 };
